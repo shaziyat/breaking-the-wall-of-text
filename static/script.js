b76e98044b3b41a78c7830e82dbeb7487c3fb9cf
shaziyat <shaziya.tambawala@gmail.com>
2016-03-16 19:02:21 -0400

    Latest

--- static/script.js
+++ static/script.js
@@ -81,4 +81,23 @@ jQuery(function($) {
   $titleEls.on('click', function() {
     $(this).next().slideToggle(200);
   });
+
+  var offset = 250;
+  var duration = 300;
+  $('.back-to-top').hide();
+   
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > offset) {
+    $('.back-to-top').fadeIn(duration);
+    } else {
+    $('.back-to-top').fadeOut(duration);
+    }
+  });
+          
+  $('.back-to-top').click(function(event) {
+    event.preventDefault();
+    $('html, body').animate({scrollTop: 0}, duration);
+    return false;
+  });
+
 });
